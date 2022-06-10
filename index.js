9107cc39f65b5d89add68580ee3ada776a2a01ad
honorbrute <106188784+honorbrute@users.noreply.github.com>
2022-06-09 22:44:43 -0400

    JSON google chrome extension
    Added:
    png icon, tv.js (Instead of grabbing from script link, grabbing from file instead), manifest.json file to open program upon new tab creation.
    
    Moved:
    HTML inline <script> element to index.js file.

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-let chart1 = document.getElementById("bitcoinChart");
-// chart1.setAttribute("lcw-coin", "RUNE");
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "COINBASE:BTCUSD",
+        "interval": "W",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "studies": [
+            "MASimple@tv-basicstudies"
+        ],
+        "container_id": "bitcoin-container"
+    }
+);
 
+
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "COINBASE:ETHUSD",
+        "interval": "W",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "ethereum-container"
+    }
+);
+
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "CAPITALCOM:DXY",
+        "interval": "W",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "dxy-container"
+    }
+);
+
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "FOREXCOM:SPXUSD",
+        "interval": "W",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "spx-container"
+    }
+);
